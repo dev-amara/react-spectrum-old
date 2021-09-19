9646f95a27d4afb546bfef860b7be460c65a855b
Amara Bamba <amara.dianney@gmail.com>
2021-09-19 02:06:39 +0000

    fix(build): add plugin to support images

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import cleaner from "rollup-plugin-cleaner";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import image from 'rollup-plugin-img';
 import packageJson from "./package.json";
 
 export default {
@@ -20,6 +21,12 @@ export default {
     },
   ],
   plugins: [
+    image({
+      output: `lib/images`, // default the root
+      extensions: /\.(png|jpg|jpeg|gif|svg)$/, // support png|jpg|jpeg|gif|svg, and it's alse the default value
+      limit: 8192,  // default 8192(8k)
+      exclude: 'node_modules/**'
+    }),
     cleaner({
       targets: ["./lib"],
     }),
